d69b5fa3cfccedf182a1076c4a71cb7e64cdeb43
Kieran Firkin <89042089+NotKieranF@users.noreply.github.com>
2024-12-01 15:27:59 -0500

    Added hexagonal rendering

--- src/map.cpp
+++ src/map.cpp
@@ -1,22 +1,50 @@
 #include "map.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 // Map will need to be fully remade to be a graph
 
+MapTile::MapTile(SDL_Color color) : color(color) {
+    return;
+}
+
 Map::Map(int width, int height) : width(width), height(height) {
     // Create a simple grid with all tiles passable
-    grid.resize(height, std::vector<int>(width, 0));
+    grid.resize(height, std::vector<MapTile>(width, MapTile({0, 0, 255, 0})));
 }
 
 void Map::render(SDL_Renderer* renderer) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            SDL_Rect tile = { x * 32, y * 32, 32, 32 };
-            SDL_RenderDrawRect(renderer, &tile);
+            // Squash grid vertically, stretch horizontally, and offset every other row by half a column
+            SDL_FPoint pos = { .x = 32 * (x * 3.0f + (y % 2) * 1.5f), 
+                               .y = 32 * (y * sqrt(0.75)) };
+            grid[y][x].render(renderer, pos, 30);
         }
     }
 }
 
 bool Map::isValidMove(int x, int y) {
-    return (x >= 0 && x < width && y >= 0 && y < height && grid[y][x] == 0);
+    return true; //(x >= 0 && x < width && y >= 0 && y < height && grid[y][x] == 0);
+}
+
+void MapTile::render(SDL_Renderer* renderer, SDL_FPoint pos, float scale) {
+    // Create array of hexagonally distributed vertices
+    SDL_Vertex vertices[6];
+    for (int i = 0; i < 6; i++) {
+        vertices[i] = { .position = { .x = pos.x + scale * (cos(i * M_PI / 3)), 
+                                      .y = pos.y + scale * (sin(i * M_PI / 3)) },
+                        .color = this->color };
+    }
+
+    // Create four triangles which cover the surface of the hexagon
+    int indices[4 * 3];
+    for (int i = 0; i < 4; i++) {
+        indices[i * 3 + 0] = 0;
+        indices[i * 3 + 1] = i + 1;
+        indices[i * 3 + 2] = i + 2;
+    }
+
+    // Render hexagon to the screen
+    SDL_RenderGeometry(renderer, nullptr, vertices, 6, indices, 12);
 }
--- src/map.h
+++ src/map.h
@@ -4,15 +4,26 @@
 #include <SDL2/SDL.h>
 #include <vector>
 
+class MapTile {
+public:
+    MapTile(SDL_Color color);
+    void render(SDL_Renderer* renderer, SDL_FPoint pos, float scale);
+
+private:
+    SDL_Color color;
+};
+
 class Map {
 public:
     Map(int width, int height);
     void render(SDL_Renderer* renderer);
     bool isValidMove(int x, int y);
-    
+    int dist(int fromX, int fromY, int toX, int toY);
+    float weightedDist(int fromX, int fromY, int toX, int toY);
+
 private:
     int width, height;
-    std::vector<std::vector<int>> grid;
+    std::vector<std::vector<MapTile>> grid;
 };
 
 #endif
